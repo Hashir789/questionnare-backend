2efb638c17134dc2c076ae0fff224149dec4c564
smallT123456 <shaffinmalik9@gmail.com>
2024-04-10 22:55:25 +0500

    Commit 3 - Resolved Deployement Issues(2)

--- backend/urls.py
+++ backend/urls.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
 from gennotate import views
+from django.http import HttpResponse
+
+def hello_world(request):
+    return HttpResponse("Hello, World!")
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('backend.api.urls')),
     path('api/get_all_users/', views.get_all_users, name='get_all_users'),
+    path('', hello_world, name='g'),
 ]
--- gennotate/views.py
+++ gennotate/views.py
@@ -2,9 +2,13 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .api.serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework import status
 
 @api_view(['GET'])
 def get_all_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users, many=True)
-    return Response(serializer.data)
+    try:
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)
+        return Response(serializer.data)
+    except Exception as e:
+        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
